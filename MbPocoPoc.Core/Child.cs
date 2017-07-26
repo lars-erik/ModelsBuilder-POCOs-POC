9d7b55b93f48e848acefc3d24d7e701c97498e6b
Lars-Erik Aabech <lea@markedspartner.no>
2017-07-26 18:05:17 +0200

    Added pocos

---
+++ MbPocoPoc.Core/Child.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MbPocoPoc.Core
+{
+    public class Child
+    {
+        public IEnumerable<string> Checkboxes { get; set; }
+    }
+}
---
+++ MbPocoPoc.Core/Parent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MbPocoPoc.Core
+{
+    public class Parent
+    {
+        public Parent()
+        {
+        }
+
+        public IHtmlString RichText { get; set; }
+    }
+}
